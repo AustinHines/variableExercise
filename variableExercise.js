19d07df22a2e5efe998603dfb35c32eabda3c116
Austin Hines <Austinhines0605@gmail.com>
2021-02-23 12:47:53 -0600

    almost finished bonus

--- variableExercise.js
+++ variableExercise.js
@@ -1,14 +1,35 @@
 //constant
 
 let num = 20;
- num = 15
- num = num + 7;
-num - 1;
+console.log(num);
+
+
+num = 15
+console.log(num);
+
+
+num += 7;
+console.log(num);
+
+num--;
+console.log(num);
+
+
+
 num = "This is not a number"
 console.log(num);
 
-let blank = " "
+let blank;
 console.log(blank);
+
+blank = " "
+console.log(blank);
+
+const quarter = 25, dime = 10, penny = 1; nickle = 5;
+console.log(quarter, dime, penny, nickle);
+
+let letters = "qwertyuiop"
+console.log(letters[letters.lenghth - 1]);
 
 
 
